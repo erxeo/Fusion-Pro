69b11a8f078da995f5fe9e2d5cc34b93020be2fe
erxeo <88217359+erxeo@users.noreply.github.com>
2026-03-22 21:47:02 -0400

    The 3rd reset (imma lock tf in)

---
+++ app/src/main/java/com/fusionpro/nle/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.fusionpro.nle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.fusionpro.nle.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'nle' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'nle' library on application startup.
+        init {
+            System.loadLibrary("nle")
+        }
+    }
+}
